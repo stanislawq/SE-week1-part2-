e0f7cf780099d50b7c9edcc027e58b2af0da5c9a
Stanislav <stas.kustov10@gmail.com>
2026-02-14 21:01:12 +0200

    week 2: task 3

--- viikko2/nhl-reader/index.py
+++ viikko2/nhl-reader/index.py
@@ -5,15 +5,20 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
     response = requests.get(url).json()
 
-    print("Players from FIN:")
-    #print(response)
-    
-    # I have removed players = [] because the list is giant
+    print("Players from FIN: \n")
+
+    players = []
+
     for player_dict in response:
         player = Player(player_dict)
         
         if player.nationality == "FIN":
-            print(player)
+            players.append(player)
+
+    players.sort(key=lambda p: p.points, reverse=True)
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/player.py
+++ viikko2/nhl-reader/player.py
@@ -5,8 +5,13 @@ class Player:
         self.assists = dict['assists']
         self.goals = dict['goals']
         self.team = dict['team']
+        #not used, but good to have
         self.games = dict['games']
         self.id = dict['id']
 
+    @property
+    def points(self):
+        return self.goals + self.assists
+
     def __str__(self):
-        return f"{self.name:20} team {self.team:3} goals {self.goals:2} assists {self.assists:2}"
+        return f"{self.name:20} {self.team} {self.goals} + {self.assists} = {self.points}"
