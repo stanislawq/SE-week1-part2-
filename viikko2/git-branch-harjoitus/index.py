16305abbdfdfd47fea6ae37fedeea92273c61772
Stanislav <stas.kustov10@gmail.com>
2026-02-15 00:52:23 +0200

    week 2: task 11 change prints to math format in main

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,12 +3,12 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan 1") # Change in main
+logger("aloitetaan 1") # change in main
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{x}+{y}={summa(x, y)}") # change in main
+print(f"{x}-{y}={erotus(x, y)}") # change in main
 
 logger("lopetetaan")
 print("goodbye!") # added in bugfix branch
