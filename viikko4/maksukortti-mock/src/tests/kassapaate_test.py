7be18881ed2e14fcca0f15e3aeda35619f4aa33b
Stanislav <stas.kustov10@gmail.com>
2026-02-22 03:37:55 +0200

    week 4: task 2

---
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kassapaate import Kassapaate, HINTA
+from maksukortti import Maksukortti
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassa = Kassapaate()
+
+    def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 10
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_called_with(HINTA)
+
+    def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 4
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_not_called()
+        
+    def test_load_positive_amount_calls_card_load_method(self):
+        payment_card_mock = Mock()
+        self.kassa.lataa(payment_card_mock, 10)
+        
+        payment_card_mock.lataa.assert_called_with(10)
+
+    def test_load_negative_amount_does_not_call_card_load_method(self):
+        payment_card_mock = Mock()
+        self.kassa.lataa(payment_card_mock, -5)
+        
+        payment_card_mock.lataa.assert_not_called()
